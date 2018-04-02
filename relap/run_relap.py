64aafdeaa742b19821ef34c7ff74d8cbaa8a9fa5
Maoxie <edwardyzt@gmail.com>
2018-04-02 10:49:02 +0800

    reformed run_relap.py

--- relap/run_relap.py
+++ relap/run_relap.py
@@ -15,11 +15,7 @@ def run_new_model(root=r".\relap_bin"):
     o_file = root_path / 'outdta'
     r_file = root_path / 'rstplt'
     for filepath in [o_file, r_file]:
-        if filepath.exists():
-            opt = input(u"是否删除已有的{}? y/n: ".format(filepath.name))
-            if opt=="" or opt.lower()=="y":
-                filepath.replace(o_file.with_name('{}.old'.format(filepath.name)))
-                print("{0} has been renamed as {0}.old.".format(filepath.name))
+        __remove_exist_file(filepath)
 
     run_options = {
         '-i': '"{}"'.format(i_file),
@@ -34,25 +30,38 @@ def run_new_model(root=r".\relap_bin"):
     subprocess.call(call_str)
     print("----complete model----")
 
+
 def run_strip(root=r".\relap_bin"):
-    root_path = Path(root)
+    root_path = Path(os.path.abspath(root))
     relap_bin_exe = root_path / 'relap5.exe'      # main relap5 executatble file
 
     i_file = root_path / 'strip'
     o_file = root_path / 'outdta'
     r_file = root_path / 'rstplt'
+    sf_file = root_path / 'stripf'
+    for filepath in [sf_file,]:
+        __remove_exist_file(filepath)
     run_options = {
-        'i': "'{}'".format(i_file),
-        'o': "'{}'".format(o_file),
-        'r': "'{}'".format(r_file),
+        '-i': "'{}'".format(i_file),
+        '-o': "'{}'".format(o_file),
+        '-r': "'{}'".format(r_file),
+        '-s': "'{}'".format(sf_file),
     }
-    options_str = '-i {i} -o {o} -r {r}'.format(
-        i=run_options['i'], 
-        o=run_options['o'], 
-        r=run_options['r'],
-    )
-
+    options_str = ' '.join([ key +' '+ value for key,value in run_options.items()])
+    call_str = '{relap} {option}'.format(relap=relap_bin_exe, option=options_str)
     print("----start strip----")
-    subprocess.call('{relap} {option}'.format(relap=relap_bin_exe, option=options_str))
+    print("-->> {}".format(call_str))
+    os.chdir(root_path)
+    subprocess.call(call_str)
     print("----complete strip----")
+
+
+def __remove_exist_file(filepath):
+    filepath = Path(filepath)
+    if filepath.exists():
+        opt = input(u"是否删除已有的{}? y/n: ".format(filepath.name))
+        if opt=="" or opt.lower()=="y":
+            filepath.replace(filepath.with_name('{}.old'.format(filepath.name)))
+            print("{0} has been renamed as {0}.old.".format(filepath.name))
+
 
